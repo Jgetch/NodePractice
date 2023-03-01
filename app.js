a9faa1c17a2555c30be331e991caeadb98ff370c
Jamie G <115777900+Jgetch@users.noreply.github.com>
2023-03-01 11:05:06 +0000

    finished exercise

--- app.js
+++ app.js
@@ -1,61 +1,11 @@
-//we want to use logger module here
-//to load the module we use the require function
-const logger = require("./logger");
-const path = require("path"); //built in PATH module stored in const path
-const os = require("os");
-const fs = require("fs");
-const EventEmitter = require("events"); // CLASS OF EVENT EMITTER
+const EventEmitter = require("events");
+const Logger = require("./logger");
 
-//when calling a single function this is best practice
-logger("This is calling a function from logger.js");
+const logger = new Logger();
 
-//when calling objects, this is best practice
-logger.logging("This is calling an object from logger.js");
-
-let pathObject = path.parse(__filename); //utilises the parse method of the path module, using the __filename argument.
-//result stored in pathObject variable
-
-console.log(pathObject); //calling pathObject as a console.log
-
-//
-//OS MODULE VARIABLES
-let totalMemory = os.totalmem();
-let freeMemory = os.freemem();
-
-//OS MODULE CALLS
-console.log(`the total memory available on this mac is ${totalMemory} bits`);
-console.log(`the free memory available on this mac is ${freeMemory} bits`);
-
-//
-//FS MODULE VARIABLES - DO NOT USE SYNCHRONOUS, ONLY ASYNCHRONOUS. (NOT SYNC)
-let fileReadSync = fs.readdirSync("./"); // this is sync
-let fileReadAsync = fs.readdir("../", function (err, files) {
-  if (err) console.log("ERROR", err);
-  else console.log("Result", files);
-}); // this is async
-
-//FS MODULE CALLS
-console.log(fileReadSync);
-console.log(fileReadAsync);
-
-//insansiating emitter object
-const emitter = new EventEmitter(); // OBECT OF EMITTER
-
-//registering a listener
-emitter.on("messageLogged", function () {
-  //this does not call an argument, and uses standard function syntax
-  console.log("Listener Called.");
+// register a listener for the "messageLogged" event
+logger.on("messageLogged", (arg) => {
+  console.log("Listener called", arg);
 });
 
-//registering another listener
-emitter.on("messageWithArgs", (arg) => {
-  //call an argument with 'arg' and uses an arrow function
-
-  console.log("The arguments are: ", arg);
-});
-
-//raising an event
-emitter.emit("messageLogged"); // signalling an event has happened
-
-//raising another event with event arguments
-emitter.emit("messageWithArgs", { id: 1, url: "http://www.google.com" }); // signalling an event has happened
+logger.log("message");
--- logger.js
+++ logger.js
@@ -1,26 +1,14 @@
-// (function (exports, require, module, __filename, __dirname) {
-//this is what node does in the background, it's called the module wrapper function.
-
-//here we create a module
-
-console.log(__filename); //logs filename of the module
-console.log(__dirname); // logs dirname of the module
+const EventEmitter = require("events");
 
 let url = "http://www.google.com";
 
-function logging(message) {
-  //sends an HTTP request
-  console.log(message);
+class Logger extends EventEmitter {
+  log(message) {
+    //send http request
+    console.log(message);
+    //raise an event
+    this.emit("messageLogged", { id: 1, url: "http://" });
+  }
 }
 
-//variable and function both scoped to logger.js if not public
-
-//to make the function and variable scoped elsewhere, use exports.
-//if needing to export one function this is best practice
-module.exports = logging;
-
-//if we need to export objects this is best practice
-module.exports.logging = logging;
-// module.exports.endPoint = url; //can change name when exporting to endPoint
-
-// });
+module.exports = Logger;
