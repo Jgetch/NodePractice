362b6de135930627c9dabf4bd18fe6f0fffa4a2e
Jamie G <115777900+Jgetch@users.noreply.github.com>
2023-03-01 12:52:10 +0000

    final

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+//imports
 const EventEmitter = require("events");
 const Logger = require("./logger");
+const http = require("http");
+const { Socket } = require("dgram");
+const fs = require("fs");
+const path = require("path");
 
+// Create an instance of the `http.Server` class
+const server = http.createServer();
+
+// Register a listener for the `connection` event on the `server` object
+server.on("connection", () => {
+  console.log("new connection");
+});
+
+// Start the server and make it listen on port 3000
+server.listen(3000);
+
+// Output a message to the console indicating that the server is running
+console.log("Listening on port 3000...");
+
+// Create an instance of the `Logger` class
 const logger = new Logger();
 
-// register a listener for the "messageLogged" event
+// Register a listener for the "messageLogged" event
 logger.on("messageLogged", (arg) => {
   console.log("Listener called", arg);
 });
 
+// Call the `log()` method of the `logger` object and pass a message as an argument
 logger.log("message");
--- logger.js
+++ logger.js
@@ -1,14 +1,20 @@
+// Importing the 'events' module
 const EventEmitter = require("events");
 
+// Initializing a 'url' variable as a str
 let url = "http://www.google.com";
 
+// Creating a new class called 'Logger' that inherits from the 'EventEmitter' class
 class Logger extends EventEmitter {
+  // Defining a 'log' method that logs the 'message' argument and emits a 'messageLogged' event
   log(message) {
-    //send http request
+    // Logging the 'message' argument
     console.log(message);
-    //raise an event
+
+    // Emitting a 'messageLogged' event with an object containing 'id' and 'url' properties
     this.emit("messageLogged", { id: 1, url: "http://" });
   }
 }
 
+// Exporting the 'Logger' class for use in other files
 module.exports = Logger;
